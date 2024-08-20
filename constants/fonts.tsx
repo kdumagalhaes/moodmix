2e3932bf64ce93d28602dd05a06f6755a288551b
Carlos Pereira <carlos.pereira@tekever.com>
2024-08-20 21:46:57 +0100

    Add Archivo in fonts constant file

--- constants/fonts.tsx
+++ constants/fonts.tsx
@@ -1,3 +1,8 @@
-import {Archivo_Black} from "next/font/google"
+import { Archivo, Archivo_Black } from "next/font/google";
 
-export const archivo_black = Archivo_Black({ subsets: ["latin"], weight: ['400'] });
+export const archivo_black = Archivo_Black({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const archivo = Archivo({ subsets: ["latin"], weight: ["400"] });
