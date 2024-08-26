4aae63c5c48168ef3031014ee41c4fa42b27cacc
Carlos Pereira <carlos.pereira@tekever.com>
2024-08-26 19:35:26 +0100

    Add Moods list

--- components/Moods/Mood/Mood.tsx
+++ components/Moods/Mood/Mood.tsx
@@ -1,18 +1,21 @@
 import { archivo } from "@/constants/fonts";
+import Link from "next/link";
 
 interface MoodProps {
   icon: string;
   text: string;
+  path: string;
 }
-export default function Mood({ icon, text }: MoodProps) {
+export default function Mood({ icon, text, path }: MoodProps) {
   return (
-    <button
+    <Link
+      href={`musics/${path}`}
       className="
       flex gap-2 w-full justify-center max-w-48 border rounded-md
     border-emerald-400 p-5 hover:bg-emerald-400 hover:text-black hover:duration-300"
     >
       <span>{icon}</span>
       <p className={`uppercase ${archivo.className}`}>{text}</p>
-    </button>
+    </Link>
   );
 }
--- components/Moods/MoodsList/MoodsList.tsx
+++ components/Moods/MoodsList/MoodsList.tsx
@@ -1,5 +1,22 @@
+import { MOODS } from "@/constants/moods";
 import Mood from "../Mood/Mood";
 
 export default function MoodsList() {
-  return <Mood icon="😊" text="happy" />;
+  return (
+    <div>
+      <p className="text-xl mt-8 mb-5 text-center">How are you feeling now?</p>
+      <div className="grid grid-cols-5 gap-4">
+        {MOODS.map((mood) => {
+          return (
+            <Mood
+              key={mood.id}
+              icon={mood.icon}
+              text={mood.text}
+              path={mood.path}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
